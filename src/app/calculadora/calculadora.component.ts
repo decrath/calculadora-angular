0b9691242a3b48338928c7683ad57b36523887a3
victor <victorcamero2000@hotmail.com>
2020-05-13 04:27:25 -0400

    Se crearon los botones de los números, para la calculadora

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-calculadora',
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculadoraComponent {
 
-  constructor() { }
+  longButtons: string[] = ['AC', 'CE'];
+  buttons: string[] = ['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '=', '+'];
 
  
 
