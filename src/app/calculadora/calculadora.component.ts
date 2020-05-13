db2a273ef5e011920ba39acf5443e969de87084c
victor <victorcamero2000@hotmail.com>
2020-05-13 06:14:51 -0400

    se implemento la logica del programa

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -7,9 +7,30 @@ import { Component } from '@angular/core';
 })
 export class CalculadoraComponent {
 
+  result: string = '';
   longButtons: string[] = ['AC', 'CE'];
-  buttons: string[] = ['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '=', '+'];
+  buttons: string[] = ['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '.', '0', '=', '+'];
 
+  private preValue: string = '';
+  private curValue: string = '';
+  
+  addToExpression(value: string) {
+    if ( this.result != '' ) {
+      this.preValue = this.curValue;
+      this.curValue = value;
+    }
+
+    if (value == 'AC'){
+      this.result = '';
+    } else if (value == 'CE') {
+      this.result = this.preValue != '=' ? this.result.slice(0, -1) : this.result;
+    } else if (value == '=') {
+      this.result = eval(this.result);
+    } else {
+      this.result += value;
+
+    }
+  }
  
 
 }
